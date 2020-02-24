6751a5503a4b03bd197802e99d41be680df3fa80
DevMoath <moath.alhajrii@gmail.com>
2020-02-24 20:39:29 +0300

    add script for delete list request

--- resources/views/app/models/delete_list.blade.php
+++ resources/views/app/models/delete_list.blade.php
@@ -13,14 +13,95 @@
                 @method('DELETE')
                 <div class="modal-body">
                     <div class="form-group">
-                        <label>Are you sure you wants to delete <kbd>{{ $list->name }}</kbd> List ?</label>
+                        <label>Are you sure you want to delete <kbd>{{ $list->name }}</kbd> List ?</label>
                     </div>
                     <div class="d-flex justify-content-end">
-                        <button type="button" class="btn text-primary btn-hover px-3 mx-2 rounded action-button animate" data-dismiss="modal">No</button>
-                        <button type="submit" class="btn btn-danger px-3 mx-2 rounded action-button animate">Yes</button>
+                        <button type="button" class="btn text-primary btn-hover px-3 mx-2 rounded action-button animate"
+                                data-dismiss="modal">
+                            No
+                        </button>
+                        <button type="submit" class="btn btn-danger px-3 mx-2 rounded action-button animate">
+                            Yes
+                        </button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
 </div>
+<script>
+    document.querySelector("#delete_list_{{ $list->id }} form").addEventListener("submit", e => {
+        e.preventDefault();
+        let url = "{{ route('list.destroy', $list->id) }}";
+        fetch(url, {
+            method: "delete",
+            headers: {
+                "Accept": "*/*",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                _token: "{{ csrf_token() }}"
+            })
+        }).then(response => {
+            if (response.ok) {
+                $("#delete_list_{{ $list->id }}").modal("hide");
+                Swal.fire({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    icon: "success",
+                    title: "List deleted"
+                });
+                document.querySelector('#list_{{ $list->id }}').remove();
+                document.querySelector("#list-tab a.active").remove();
+                let listTab = document.querySelector("#list-tab");
+                if (listTab.children.length === 0) {
+                    document.querySelector("#nav-tabContent").innerHTML =
+                        "<div class='text-center mx-auto w-75 alert shadow-lg my-3 alert-dismissible fade show shadow'" +
+                        "     role='alert' id=''>" +
+                        "    <div class='card-body'>" +
+                        "        <button class='btn close float-right btn-hover rounded animate action-button' data-dismiss='alert'>" +
+                        "            <i class='fa fa-times fa-fw'></i>" +
+                        "        </button>" +
+                        "        <h5 class='card-title'>Ooops</h5>" +
+                        "        <p class='card-text'>You don't have list.</p>" +
+                        "        <button type='button' onclick='document.getElementById(\"add_list_button\").click()'" +
+                        "                class='btn btn-primary rounded animate action-button px-3 mx-2 shadow'>" +
+                        "            Create One Now!" +
+                        "        </button>" +
+                        "    </div>" +
+                        "</div>";
+                    listTab.innerHTML                                   =
+                        "<div class='text-center mx-auto w-100 alert shadow-lg my-3 alert-dismissible fade show shadow'" +
+                        "     role='alert' id=''>" +
+                        "    <div class='card-body'>" +
+                        "        <button class='btn close float-right btn-hover rounded animate action-button' data-dismiss='alert'>" +
+                        "            <i class='fa fa-times fa-fw'></i>" +
+                        "        </button>" +
+                        "        <h5 class='card-title'>Ooops</h5>" +
+                        "        <p class='card-text'>You don't have list.</p>" +
+                        "        <button type='button' onclick='document.getElementById(\"add_list_button\").click()'" +
+                        "                class='btn btn-primary rounded animate action-button px-3 mx-2 shadow'>" +
+                        "            Create One Now!" +
+                        "        </button>" +
+                        "    </div>" +
+                        "</div>";
+                } else {
+                    listTab.children[0].click();
+                }
+            } else {
+                Swal.fire({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    icon: "error",
+                    title: "List can't be deleted, Try again"
+                });
+            }
+        }).catch(error => console.error(error));
+    });
+</script>
