fb2e0aed2c57c986d165bbc861cf41aaaa1d2183
DevMoath <moath.alhajrii@gmail.com>
2020-03-08 19:16:02 +0300

    update navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,50 +2,50 @@
     <div class="container">
         <div>
             @if(Route::getCurrentRoute()->getName() === 'home')
-                <a class="navbar-brand animate btn-hover round px-3 mx-0" href="#">
+                <a class="navbar-brand animate btn-hover rounded px-3 mx-0" href="#">
                     {{ config('app.name', 'Todoit') }}
                 </a>
             @else
-                <a class="navbar-brand animate btn-hover round px-3 mx-0" href="{{ route('home') }}">
+                <a class="navbar-brand animate btn-hover rounded px-3 mx-0" href="{{ route('home') }}">
                     {{ config('app.name', 'Todoit') }}
                 </a>
             @endif
         </div>
-        {{--            <button class="navbar-toggler" type="button" data-toggle="collapse"--}}
-        {{--                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"--}}
-        {{--                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
-        {{--                <span class="navbar-toggler-icon"></span>--}}
-        {{--            </button>--}}
+        {{--<button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>--}}
         <ul class="navbar-nav ml-auto">
             <li class="nav-item mx-1">
                 @auth
-                    <a href="{{ route('app.index') }}" class="nav-link btn-hover round px-3 animate">
+                    <a href="{{ route('app.index') }}" class="nav-link btn-hover rounded px-3 animate">
                         @lang('Start Now')
                     </a>
                 @else
-                    <a href="{{ route('login') }}" class="nav-link btn-hover round px-3 animate">
+                    <a href="{{ route('login') }}" class="nav-link btn-hover rounded px-3 animate">
                         @lang('Start Now')
                     </a>
                 @endauth
             </li>
         </ul>
-        {{--            <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-        {{--                <!-- Left Side Of Navbar -->--}}
-        {{--                <ul class="navbar-nav ml-auto"></ul>--}}
-        {{--                <!-- Right Side Of Navbar -->--}}
-        {{--                <ul class="navbar-nav ml-auto">--}}
-        {{--                    <li class="nav-item mx-1">--}}
-        {{--                        @auth--}}
-        {{--                            <a href="{{ route('app.index') }}" class="nav-link btn-hover round px-3 animate">--}}
-        {{--                                @lang('Start Now')--}}
-        {{--                            </a>--}}
-        {{--                        @else--}}
-        {{--                            <a href="{{ route('login') }}" class="nav-link btn-hover round px-3 animate">--}}
-        {{--                                @lang('Start Now')--}}
-        {{--                            </a>--}}
-        {{--                        @endauth--}}
-        {{--                    </li>--}}
-        {{--                </ul>--}}
-        {{--            </div>--}}
+        {{--<div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav ml-auto"></ul>
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item mx-1">
+                    @auth
+                        <a href="{{ route('app.index') }}" class="nav-link btn-hover round px-3 animate">
+                            @lang('Start Now')
+                        </a>
+                    @else
+                        <a href="{{ route('login') }}" class="nav-link btn-hover round px-3 animate">
+                            @lang('Start Now')
+                        </a>
+                    @endauth
+                </li>
+            </ul>
+        </div>--}}
     </div>
 </nav>
